56cac2e947a19c7062e8642ba6404c2cf0867099
matthu24 <matthu24@gmail.com>
2017-10-10 15:30:44 -0700

    pawn valid_moves working

--- W2D1/chess/pawn.rb
+++ W2D1/chess/pawn.rb
@@ -9,8 +9,39 @@ class Pawn < Piece
 
 protected
   def move_dirs
-    # for now just put 1,0
-    # TODO:
-    moves = [[1,0]]
+    if color == :black
+      # first move
+      if pos[0] == 1
+        directions = [[1,0],[2,0]]
+      else
+        directions = [[1,0]]
+      end
+      # enemy on lower left diag
+      if !@board[[pos[0] + 1,pos[1] - 1]].is_a?(NullPiece) && @board[[pos[0] + 1,pos[1] - 1]].color == :white
+        directions << [1,-1]
+      end
+      # enemy on lower right diag
+      if !@board[[pos[0] + 1,pos[1] + 1]].is_a?(NullPiece) && @board[[pos[0] + 1,pos[1] + 1]].color == :white
+        directions << [1,1]
+      end
+    #for white
+    else
+      if pos[0] == 6
+        directions = [[-1,0],[-2,0]]
+      else
+        directions = [[-1,0]]
+      end
+      # enemy on upper left diag
+      if !@board[[pos[0] - 1,pos[1] - 1]].is_a?(NullPiece) && @board[[pos[0] - 1,pos[1] - 1]].color == :black
+        directions << [-1,-1]
+      end
+      # enemy on upper right diag
+      if !@board[[pos[0] - 1,pos[1] + 1]].is_a?(NullPiece) && @board[[pos[0] - 1,pos[1] + 1]].color == :black
+        directions << [-1,1]
+      end
+    end
+    directions
   end
+
+
 end
