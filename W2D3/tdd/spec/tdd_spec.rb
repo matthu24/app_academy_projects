14838dd7433aa5809f8c362a32efd9471648d241
App Academy Student <student@appacademy.io>
2017-10-11 15:11:02 -0700

    Finish TDD project

--- W2D3/tdd/spec/tdd_spec.rb
+++ W2D3/tdd/spec/tdd_spec.rb
@@ -1,5 +1,6 @@
 require 'tdd'
 require 'rspec'
+require 'byebug'
 
 RSpec.describe Array do
 
@@ -60,8 +61,100 @@ RSpec.describe Array do
     end
   end
 
+  describe "#stock_picker" do
+    subject(:arr) {[0,1,10,-2]}
+    let(:result) {arr.stock_picker}
+    let(:answer) {[0,2]}
+
+    it "chooses the most profitable days to buy and sell" do
+      expect(result).to eq(answer)
+    end
+
+    it "does not buy after it sells" do
+      expect(answer[0]).to be <(answer[1])
+    end
 
 
+  end
+
+end
+
+RSpec.describe TowersOfHanoiGame do
+  subject(:game) { TowersOfHanoiGame.new }
+  #game does not initialize until it statement
+  #subject is a lazy initializer, it does not initalize the object
+  #until an it satement
+
+  let(:board) { game.board }
+  describe "#initialize" do
+    it "makes an array of three arrays" do
+      expect(board.length).to eq(3)
+    end
+
+    it "sets all discs on leftmost peg" do
+      expect(board[0].length).to eq(3)
+    end
+
+    it "sets other pegs to be empty" do
+      expect(board[1]).to be_empty
+      expect(board[2]).to be_empty
+    end
+  end
+
+  describe "#move" do
+
+    before(:each) do
+
+      game.board = [[3],[2,1],[]]
+    end
+
+    it "moves disk to empty peg" do
+
+      game.move(0,2)
+
+      expect(board).to eq([[],[2,1],[3]])
+    end
+
+    it "moves disk onto bigger disk" do
+      game.move(1,0)
+      expect(board).to eq([[3,1], [2], []])
+    end
+
+    it "cannot move from empty peg" do
+
+      expect{game.move(2,0)}.to raise_error "Empty peg!"
+    end
+
+    it "shall not move bigger disk onto smaller disk" do
+      expect{game.move(0,1)}.to raise_error "Invalid move!"
+    end
+
+  end
+
+  describe "#win?" do
+    let(:win_one) do
+      game.board = [[],[3,2,1],[]]
+    end
+    let(:win_two) do
+      game.board = [[],[],[3,2,1]]
+    end
+    let(:not_win) do
+      game.board = [[],[1],[3,2]]
+    end
+    it "returns true if game won" do
+      win_one
+      expect(game.win?).to be true
+      not_win
+      expect(game.win?).to be false
+      win_two
+      expect(game.win?).to be true
+    end
+
+    it "returns false if game is not won" do
+      not_win
+      expect(game.win?).to be false
+    end
+  end
 
 
 
