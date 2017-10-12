f25d962b9a37aab0964b3b2498b4f55292fd48eb
App Academy Student <student@appacademy.io>
2017-10-11 17:54:08 -0700

    Add rspec tests for poker

--- W2D3/poker/spec/poker_spec.rb
+++ W2D3/poker/spec/poker_spec.rb
@@ -13,9 +13,39 @@ RSpec.describe Poker do
     end
   end
 
-  describe "#deal" do
-    it "distributes five cards to each player"
+  describe "#deal_hand" do
+    it "distributes 5 cards to player" do
+      players = game.players
+      game.deal_hands
+      expect(players.all? { |player| player.hand.length == 5 }).to be true
+      expect(game.deck.length).to eq(27)
+    end
 
+  end
+  describe "#redeal_cards" do
+    it "distributes maximum 3 cards to player during round" do
+      expect { game.redeal_cards(4, 0) }.to raise_error "Too many cards!"
+    end
+
+  end
+  describe "#deal" do
+    it "deals one card to one player at specified index" do
+      player = game.players[0]
+      deck = game.deck
+      game.deal(0)
+      expect(player.hand.length).to eq(1)
+      expect(deck.length).to eq(51)
+    end
+
+  end
+
+  describe "#prompt_player" do
+
+    it "requests a move from the player" do
+      player = double("player")
+      #allow(book).to receive(:title) { "The RSpec Book" }
+      allow(player).to receive(:get_move)
+    end
   end
 
 end
