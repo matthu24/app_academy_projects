2d3f1d48a6ce6ac846685ce7df97c7c69095eb68
matthu24 <matthu24@gmail.com>
2017-10-11 17:54:08 -0700

    Add rspec tests for poker

--- W2D3/poker/lib/poker.rb
+++ W2D3/poker/lib/poker.rb
@@ -9,6 +9,26 @@ class Poker
 
 
   end
+  def deal_hands
+    @players.each_index do |player_idx|
+      5.times do
+        deal(player_idx)
+      end
+    end
+  end
+
+  def redeal_cards(num_cards, player_idx)
+    raise "Too many cards!" if num_cards > 3
+
+  end
+
+  def deal(player_idx)
+    @players[player_idx].hand << @deck.pop
+  end
+
+  def prompt_player(player_idx)
+    @players[player_idx].get_move
+  end
 
   def self.init_players(num_players)
     players = []
@@ -16,7 +36,6 @@ class Poker
       players << Player.new(@deck)
     end
     players
-
   end
 
 
